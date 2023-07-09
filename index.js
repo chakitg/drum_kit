d62e5d47760d2b7b79c9a617a47e6023c4394c0d
chakitg <chakitg1@gmail.com>
2023-07-09 14:20:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I got clicked!");
+    });
+}
+
+
+
